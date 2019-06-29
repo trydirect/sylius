0a1278c885e77a83ea352ad2ab42a63416adc0a8
vsilent <jabberroid@gmail.com>
2019-06-29 14:05:11 +0300

    docker-compose down before build

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+docker-compose -f docker-compose-build.yml down
+rm -rf ./configs/nginx/conf.d/.htpasswd
 printf "admin:`echo "admin" | openssl passwd -stdin`" > ./configs/nginx/conf.d/.htpasswd
 cp ./configs/sylius/.env ./sylius/.env
+
 docker-compose -f docker-compose-build.yml up -d
 docker exec -i php composer dump-env dev
 docker exec -i php php bin/console sylius:install -q -n
