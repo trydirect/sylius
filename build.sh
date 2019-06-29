1391103d23e217ba353d6f2ea19a5fd59e269df5
vsilent <jabberroid@gmail.com>
2019-06-29 13:23:31 +0300

    build.sh for travis, remove DS_Store

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+printf "admin:`echo "admin" | openssl passwd -stdin`" > ./configs/nginx/conf.d/.htpasswd
+cp ./configs/sylius/.env ./sylius/.env
+docker-compose -f docker-compose-build.yml up -d
+docker exec -i php composer dump-env dev
+docker exec -i php php bin/console sylius:install -q -n
+docker-compose run --rm --user=root node yarn install
+docker-compose run --rm --user=root node yarn build
